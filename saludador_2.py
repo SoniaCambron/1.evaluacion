50f1edd15f61d33fe00c359e559ea8c5a21b412c
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-09-26 09:42:20 +0200

    Add files via upload

---
+++ saludador_2.py
@@ -0,0 +1,6 @@
+def saludador_2():
+    print "Hola amigo" #Instruccion de salida
+    nombre= raw_input("Como te llamas?: ") #Instrucción de entrada
+    print "Bienvenido a Python "+ nombre
+
+saludador_2()
