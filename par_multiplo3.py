479168c312c03ea7bda4d9fadb46bb2aacb34638
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-10-15 11:32:18 +0200

    Add files via upload

---
+++ par_multiplo3.py
@@ -0,0 +1,14 @@
+def par_multiplo3():
+    print "dime un número"
+    a=input("A= ")
+    if (a%2==0):
+        print "par"
+    else:
+        print "impar"
+    if (a%3==0):
+        print "multiplo de 3"
+    else:
+        print "no es multiplo de 3"
+
+par_multiplo3()
+    
