91d68d49e56eb6371b1169dc04703ce1c09fe45a
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-10-18 09:21:58 +0200

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+def ejercicio1():
+    print"Dime cuatro numeros distintos de 0"
+    a=input("A= ")
+    b=input("B= ")
+    c=input("C= ")
+    d=input("D= ")
+    multiplicacion1=a*b*c*d
+    print multiplicacion1
+
+ejercicio1()
---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+def ejercicio2():
+    print"Cuantos dolares quieres cambiar?"
+    a=input("A= ")
+    dolar=0.90
+    multiplicacion= a*dolar
+    print multiplicacion
+
+ejercicio2()
