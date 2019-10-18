f83887e7f95e79c4b0063344447bbd75df9a6228
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-10-18 09:49:05 +0200

    Add files via upload

---
+++ ejercicio5.py
@@ -0,0 +1,35 @@
+def ejercicio5():
+    print"Dime un día"
+    a=input("A= ")
+    print"Dime un numero de mes"
+    b=input("B= ")
+    print"Dime un año"
+    c=input("C= ")
+    if(b==1):
+        b="Enero"
+    if(b==2):
+        b="Febrero"
+    if(b==3):
+        b="Marzo"
+    if(b==4):
+        b="Abril"
+    if(b==5):
+        b="Mayo"
+    if(b==6):
+        b="Junio"
+    if(b==7):
+        b="Julio"
+    if(b==8):
+        b="Agosto"
+    if(b==9):
+        b="Septiembre"
+    if(b==10):
+        b="Octubre"
+    if(b==11):
+        b="Noviembre"
+    if(b==12):
+        b="Diciembre"
+     print a, "de", b, "de", c
+
+    ejercicio5()
+            
