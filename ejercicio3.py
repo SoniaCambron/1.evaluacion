8ac4a715a9417e4878163e6c4c04a445634e04b2
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-10-18 09:47:35 +0200

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,19 @@
+def ejercicio3():
+    print"Dime un numero del 1 al 7"
+    a=input("A= ")
+    if(a==1):
+        print"Lunes"
+    if(a==2):
+        print"Martes"
+    if(a==3):
+        print"Miercoles"
+    if(a==4):
+        print"Jueves"
+    if(a==5):
+        print"Viernes"
+    if(a==6):
+        print"Sabado"
+    if(a==7):
+        print"Domingo"
+
+ejercicio3()
---
+++ ejercicio4.py
@@ -0,0 +1,29 @@
+def ejercicio4():
+    print"Dime un numero del 1 al 12"
+    a=input("A= ")
+    if(a==1):
+        print"Enero"
+    if(a==2):
+        print"Febrero"
+    if(a==3):
+        print"Marzo"
+    if(a==4):
+        print"Abril"
+    if(a==5):
+        print"Mayo"
+    if(a==6):
+        print"Junio"
+    if(a==7):
+        print"Julio"
+    if(a==8):
+        print"Agosto"
+    if(a==9):
+        print"Septiembre"
+    if(a==10):
+        print"Octubre"
+    if(a==11):
+        print"Noviembre"
+    if(a==12):
+        print"Diciembre"
+
+ejercicio4()
