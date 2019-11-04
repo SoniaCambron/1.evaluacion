df7d016095e144947fff7e309f416c536e5a844e
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-11-04 20:20:38 +0100

    Add files via upload

---
+++ ejercicio9.py
@@ -0,0 +1,10 @@
+def ejercicio9():
+    print"Cuantas horas has trabajado este mes?"
+    a=input("= ")
+    while (a>0):
+        for i in range (1,30):
+            suma1=i*30
+        for c in range (31,a):
+            suma2=c*45
+        print suma1+suma2
+ejercicio9()
