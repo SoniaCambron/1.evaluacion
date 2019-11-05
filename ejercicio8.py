97e9bf55673481efc453fc89193f2c3bcbe9d190
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-11-05 11:31:39 +0100

    Add files via upload

--- ejercicio8.py
+++ ejercicio8.py
@@ -2,15 +2,14 @@ def ejercicio8():
     print"De que cifra quieres sacar el IVA?"
     a=input("A= ")
     print "Que tipo de IVA vamos a aplicarle?"
-    b=input("= ")
+    b=raw_input("= ")
 
     if (b=="general"):
         b=16
-            
     if (b=="reducido"):
         b=7
     if (b=="superreducido"):
         b=4
-    precio=a+a*(b/100)
+    precio=a+a*(0.01*b)
     print precio, "euros"
 ejercicio8()
