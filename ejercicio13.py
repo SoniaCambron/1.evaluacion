c4a681b1d403471e2897245000aa36fc77c2bc34
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-11-12 11:32:27 +0100

    Add files via upload

---
+++ ejercicio13.py
@@ -0,0 +1,63 @@
+def ejercicio13():
+    h=input("H= ")
+    for filas in range (1,h+1):
+        for columnas in range (1,2*h):
+            if(filas==1)and(columnas==1):
+                print" "
+            if(filas==1) and (columnas==1+1):
+                print " "
+            if(filas==1) and (columnas==3):
+                print" "
+            if(filas==1)and(columnas==4):
+                print"*"
+            if(filas==1) and (columnas==5):
+                print " "
+            if(filas==1) and (columnas==6):
+                print" "
+            if(filas==1)and(columnas==7):
+                print" "
+            if(filas==2) and (columnas==1):
+                print " "
+            if(filas==2) and (columnas==2):
+                print" "
+            if(filas==2) and (columnas==3):
+                print"*"
+            if(filas==2)and(columnas==4):
+                print"*"
+            if(filas==2) and (columnas==5):
+                print "*"
+            if(filas==2) and (columnas==6):
+                print" "
+            if(filas==2)and(columnas==7):
+                print" "
+            if(filas==3) and (columnas==1):
+                print " "
+            if(filas==3) and (columnas==2):
+                print"*"
+            if(filas==3) and (columnas==3):
+                print"*"
+            if(filas==3)and(columnas==4):
+                print"*"
+            if(filas==3) and (columnas==5):
+                print "*"
+            if(filas==3) and (columnas==6):
+                print"*"
+            if(filas==3)and(columnas==7):
+                print" "
+            if(filas==4) and (columnas==1):
+                print "*"
+            if(filas==4) and (columnas==2):
+                print"*"
+            if(filas==4) and (columnas==3):
+                print"*"
+            if(filas==4)and(columnas==4):
+                print"*"
+            if(filas==4) and (columnas==5):
+                print "*"
+            if(filas==4) and (columnas==6):
+                print"*"
+            if(filas==4)and(columnas==7):
+                print"*"
+
+ejercicio13()
+    #nºcolumnas=2*h-1
