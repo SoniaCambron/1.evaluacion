13928591f26a6353e012085eb9972b088c51a85f
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-11-19 11:42:58 +0100

    Add files via upload

--- antoniopiramide.py
+++ antoniopiramide.py
@@ -1,14 +1,23 @@
-def piramide(filas=7):
+def piramide2():
+    filas=input("Dime la altura de la piramide: ")
     espacios=' '
     asteriscos='*'
     for i in range(filas):
-        for nespacios in range(1,filas-i-1):
+    #Constuimos la secuencia de espacios
+        for nespacios in range(1,filas-i-1): #hacemos muchas cuentas para unirlo
+        #Cuando i=1 y tenemos 4 filas nespacios ira de 1 a 2 y se suman espacios
+        #dependiendo de ese numero, por lo tanto aqui habra 2 espacios (uno del 1
+        #y despues se suma otro por el dos.
             espacios=espacios+' '
-        for nasteriscos in range(1,2*i):
+        for nasteriscos in range(1,2*i): #cuando tenemos i=1 nos va a dar 2, pero ese
+        #numero no lo cnatara asi que la relacion es una mas.
             asteriscos=asteriscos+'*'
+        #escribo de golpe toda la fila
         print  espacios + asteriscos
         espacios=' '
         asteriscos='*'
+        #paso a la siguiente fila.
+#se vaya sumando un espacio mas i=1 nespacios=3
 
-
-piramide(7)
+piramide2()
+        
