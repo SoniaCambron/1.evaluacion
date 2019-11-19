6ceff61c94821c72ab3fcde434f750f2e71fdd22
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-11-19 12:01:55 +0100

    Add files via upload

---
+++ pajarita.py
@@ -0,0 +1,18 @@
+def pajarita():
+    filas=input("Dime cuanta altura y ancho quieres: ")
+    espacios=" "
+    asterisco="*"
+    for i in range (0,filas/2):
+        for nespacios in range (i,0):
+            espacios=espacios+" "+" "
+        for nasteriscos in range(0,i):
+            asterisco=asterisco+"*"
+        
+        if(filas==filas/2):
+            asterisco=asterisco*filas
+            espacio=0
+        print asterisco+espacios+asterisco
+        espacios=" "
+        asterisco="*"
+pajarita()
+    
