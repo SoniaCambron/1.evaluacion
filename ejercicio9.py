f53f352089e19c49011f6f887fb41e8221794c8c
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-11-25 21:27:32 +0100

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -2,9 +2,9 @@ def ejercicio9():
     print"Cuantas horas has trabajado este mes?"
     a=input("= ")
     while (a>0):
-        for i in range (1,30):
+        for i in range (1,41):
             suma1=i*30
-        for c in range (31,a):
-            suma2=c*45
+        for c in range (41,a):
+            suma2=(c-40)*45
         print suma1+suma2
 ejercicio9()
